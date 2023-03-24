d464b7925901a3cd269480065797a7a220a7d8e1
Mingyu3 <aeneascon@gmail.com>
2023-03-25 00:38:29 +0900

    routing naming

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,12 +1,34 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 function Header() {
+	const router = useRouter();
+
+	const navItems = [
+		{ title: 'Home', path: '/' },
+		{ title: 'Naming', path: '/naming' },
+	];
+
+	console.log(router.pathname);
 	return (
 		<header className='p-3 bg-zinc-700 text-gray-100 flex items-center'>
-			<h1 className='font-bold mr-5'>AI ODDS</h1>
+			<h1 className='font-bold mr-5'>
+				<Link href='/'>AI ODDS</Link>
+			</h1>
 			<nav>
-				<ul className='text-sm text-zinc-300 hover:text-zinc-400 cursor-pointer transition duration-150 ease-out'>
-					<li>Naming</li>
+				<ul className='flex text-sm text-zinc-300 '>
+					{navItems.map((item) => (
+						<li>
+							<Link
+								href={item.path}
+								className={`mr-2 hover:text-zinc-400 transition duration-150 ease-out ${
+									router.asPath === item.path ? 'font-bold' : ''
+								}`}>
+								{item.title}
+							</Link>
+						</li>
+					))}
 				</ul>
 			</nav>
 		</header>
